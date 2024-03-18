9c55070b535f1eaed1636267e43587ed1e9fd983
erict875 <erict875@tuta.io>
2024-03-18 16:13:51 +0000

    Added changes to support the latest nostr-java version 0.4-SNAPSHOT

--- src/main/java/client/NostrClient.java
+++ src/main/java/client/NostrClient.java
@@ -7,14 +7,12 @@ import nostr.api.NIP01;
 import nostr.api.NIP04;
 import nostr.api.NIP44;
 import nostr.base.PrivateKey;
-import nostr.event.BaseTag;
 import nostr.event.impl.DirectMessageEvent;
 import nostr.event.impl.EncryptedPayloadEvent;
 import nostr.event.impl.TextNoteEvent;
 import nostr.event.list.PublicKeyList;
 import nostr.id.Identity;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -31,25 +29,26 @@ public class NostrClient {
 
         var relays = Map.of("local relay", "ws://localhost:5555/", "nostr band", "wss://relay.nostr.band/");
 
-        // Create the event with the default identity, sign and send to the relays
+        // Create the event with a random identity, sign and send to the relays
         var sender = Identity.getInstance(PrivateKey.generateRandomPrivKey());
         NIP01<TextNoteEvent> nip01 = new NIP01<>(sender);
         nip01.createTextNoteEvent("Hello Nostr World!").sign().send(relays);
 
-        // Create an event with a specific identity
+        // Create an event with a random identity, and an e-tag, sign and send to the relays
         sender = Identity.generateRandomIdentity();
         var nip01_ = new NIP01<>(sender);
         nip01_.createTextNoteEvent("Bonjour Nostr Monde!").addTag(NIP01.createEventTag(nip01.getEvent().getId())).signAndSend(relays);
 
-        // Create an event with a random identity and tags, sign and send to the default relay
+        // Create an event with a specific identity and p-tag (the recipient), sign and send to the default relay
         sender = Identity.getInstance(new PrivateKey("8c26b94af1a35b5e47ee2555f98602237767747e79c91909c61963e49140f46f"));
         var recipient = NIP01.createPubKeyTag(Identity.generateRandomIdentity().getPublicKey());
         nip01.createTextNoteEvent("Hola Nostr Mundo!").setRecipient(Identity.generateRandomIdentity().getPublicKey()).signAndSend(relays);
 
-        // Send a DM
+        // Send a NIP-04 DM
         var nip04 = new NIP04<DirectMessageEvent>(sender, recipient.getPublicKey());
         nip04.createDirectMessageEvent("Guten Tag Nostr Welt!").signAndSend(relays);
 
+        // Send a NIP-44 DM
         var nip44 = new NIP44<EncryptedPayloadEvent>(sender, recipient.getPublicKey());
         nip44.createDirectMessageEvent("Ciao Nostr Mondo!").signAndSend(relays);
 
